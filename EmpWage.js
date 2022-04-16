c498027b1e1c2da5392417e974bffcd393fa30d8
sonu1239 <ss316742@gmail.com>
2022-04-16 09:49:44 +0530

    check employee is present or not

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,12 @@
 console.log("welcome to EmployeeWage program");
+//constant variable
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*10) % 2;
+if (empCheck == IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else 
+{
+    console.log("Employee is Present");
+}
