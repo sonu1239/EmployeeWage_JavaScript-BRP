6d06b1b34c9da8cc4b245791419f345eb3c5b14c
sonu1239 <ss316742@gmail.com>
2022-04-17 19:34:09 +0530

    Calculate daily employee Wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,12 +1,37 @@
-console.log("welcome to EmployeeWage program");
+// console.log("welcome to EmployeeWage program");
+// //constant variable
+// const IS_ABSENT = 0;
+// let empCheck = Math.floor(Math.random()*10) % 2;
+// if (empCheck == IS_ABSENT)
+// {
+//     console.log("Employee is Absent");
+// }
+// else 
+// {
+//     console.log("Employee is Present");
+// }
+
+//uc-2 DailyEmpWage
+
 //constant variable
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random()*10) % 2;
-if (empCheck == IS_ABSENT)
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10) % 3;
+switch (empCheck)
 {
-    console.log("Employee is Absent");
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
 }
-else 
-{
-    console.log("Employee is Present");
-}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage : " +empWage);
+
+
