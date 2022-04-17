e8317cdcaca952985de8b75fbb2ad3617bff8cc8
sonu1239 <ss316742@gmail.com>
2022-04-17 19:45:19 +0530

    Calculate daily employee Wage using function

--- EmpWage.js
+++ EmpWage.js
@@ -14,24 +14,54 @@
 //uc-2 DailyEmpWage
 
 //constant variable
+// const IS_FULL_TIME = 1;
+// const IS_PART_TIME = 2;
+// const WAGE_PER_HOUR = 20;
+
+// let empHrs = 0;
+// let empCheck = Math.floor(Math.random()*10) % 3;
+// switch (empCheck)
+// {
+//     case IS_FULL_TIME:
+//         empHrs = 8;
+//         break;
+//     case IS_PART_TIME:
+//         empHrs = 4;
+//         break;
+//     default:
+//         empHrs = 0;
+// }
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Employee Wage : " +empWage);
+
+
+
+//UC-3 Daily Employee Wage Using function.
+
+//constant variables
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
-const WAGE_PER_HOUR = 20;
+const WAGE_PER_HOURS = 20;
+
+//This function for return working hours
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_FULL_TIME:
+            return 8;
+        case IS_PART_TIME:
+            return 4;
+        default:
+            return 0;
+    }
+}
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10) % 3;
-switch (empCheck)
-{
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    default:
-        empHrs = 0;
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage : " +empWage);
-
-
+//gives random value
+let empCheck = Math.floor(Math.random() * 10) % 3;
+//calling function
+empHrs = getWorkingHours(empCheck);
+//calculating employee wage
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("Employee Wage: " + empWage);
